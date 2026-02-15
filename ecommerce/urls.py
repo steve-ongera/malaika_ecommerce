ca99bcda0f74dfd97a2c26463bf1ae3434942ef2
steve-ongera <gadafisteve001@gmail.com>
2026-02-15 11:59:26 +0300

    added apps urls

---
+++ ecommerce/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('products/', views.product_list, name='product_list'),
+    path('product/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('category/<slug:slug>/', views.category_detail, name='category_detail'),
+    path('cart/', views.cart_view, name='cart'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('update-cart/<int:item_id>/', views.update_cart, name='update_cart'),
+    path('remove-from-cart/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('create-order/', views.create_order, name='create_order'),
+    path('order-success/<int:order_id>/', views.order_success, name='order_success'),
+    path('order-history/', views.order_history, name='order_history'),
+    path('order/<int:order_id>/', views.order_detail, name='order_detail'),
+]
